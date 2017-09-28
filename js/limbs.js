7a1e83229d7b80c7fb44909fae5a4073a22e0484
Michael Savchuk <pixelrobin@hotmail.com>
2017-09-28 09:19:03 -0700

    Add basic navbar
    No mobile support yet

---
+++ js/limbs.js
@@ -0,0 +1,24 @@
+window.limbs = new function() {
+    this.expand = function( id ) {
+        var
+            el = document.getElementById(id),
+            classes, classIndex;
+
+        if ( el ) {
+            if ( el.className.indexOf( "expanded" ) === -1 ) {
+                el.className += " expanded";
+            } else {
+                classes = el.className.split( " " ),
+                classIndex = classes.indexOf( "expanded" );
+
+                if ( classIndex !== -1 ) {
+                    classes.splice( classIndex, 1 );
+                    el.className = classes.reduce( ( a, b ) => {
+                        return a + " " + b;
+                    }, "" );
+                } else console.error( "Class does not contain 'expanded'. This shouldn't happen." );
+            }
+        } else console.error( "Error expanding. Element with id '" + id + "' does not exist." );
+    }
+
+}
